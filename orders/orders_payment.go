1a34ce28708bcade9c4d79e6d247fb1994cb1780
Daniel Hoffmann <oralordos@gmail.com>
2018-01-12 15:47:28 -0800

    Add payment details to pay order response

--- orders/orders_payment.go
+++ orders/orders_payment.go
@@ -3,14 +3,15 @@ package orders
 import "time"
 
 type PayOrderResponse struct {
-	OrderID       string             `json:"order_id"`
-	Status        OrderStatusData    `json:"status"`
-	Intent        OrderIntentData    `json:"intent"`
-	PayerInfo     *PayerInfoData     `json:"payer_info"`
-	PurchaseUnits []PurchaseUnitData `json:"purchase_units"`
-	CreateTime    time.Time          `json:"create_time"`
-	UpdateTime    time.Time          `json:"update_time"`
-	Links         []LinkData         `json:"links"`
+	OrderID        string              `json:"order_id"`
+	Status         OrderStatusData     `json:"status"`
+	Intent         OrderIntentData     `json:"intent"`
+	PayerInfo      *PayerInfoData      `json:"payer_info"`
+	PurchaseUnits  []PurchaseUnitData  `json:"purchase_units"`
+	CreateTime     time.Time           `json:"create_time"`
+	UpdateTime     time.Time           `json:"update_time"`
+	Links          []LinkData          `json:"links"`
+	PaymentDetails *PaymentDetailsData `json:"payment_details"`
 }
 
 type ResponsePreferenceData string
